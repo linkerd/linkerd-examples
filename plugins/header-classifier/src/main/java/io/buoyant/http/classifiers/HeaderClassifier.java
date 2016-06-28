0e56a19a306d5d58edade00f5c4873107ed94ae5
Alex Leong <alex@buoyant.io>
2016-06-28 10:08:46 -0700

    Add comments

--- plugins/header-classifier/src/main/java/io/buoyant/http/classifiers/HeaderClassifier.java
+++ plugins/header-classifier/src/main/java/io/buoyant/http/classifiers/HeaderClassifier.java
@@ -6,13 +6,27 @@ import com.twitter.finagle.service.ResponseClass;
 import com.twitter.finagle.service.ResponseClass$;
 import com.twitter.util.Function;
 
+/**
+ * HeaderClassifier is an HTTP response classifier that classifies responses
+ * based on the value of a response header.  If the header value is "success",
+ * the response is classified as a success.  If the header value is "retry",
+ * the response is classified as a retryable failure.  Otherwise, the response
+ * is classified as a non-retryable failure.
+ */
 public class HeaderClassifier extends Function<ReqRep, ResponseClass> {
     private String headerName;
 
+    /**
+     * @param headerName the name of the response header to use
+     */
     public HeaderClassifier(String headerName) {
         this.headerName = headerName;
     }
 
+    /**
+     * This defines which inputs this classifier accepts.  We accept any
+     * response where the header is present.
+     */
     @Override
     public boolean isDefinedAt(ReqRep reqRep) {
         if (reqRep.response().isThrow()) return false;
@@ -21,6 +35,9 @@ public class HeaderClassifier extends Function<ReqRep, ResponseClass> {
         return rep.headerMap().get(headerName).isDefined();
     }
 
+    /**
+     * Classify responses based on the header.
+     */
     @Override
     public ResponseClass apply(ReqRep reqRep) {
         Response rep = (Response) reqRep.response().get();
