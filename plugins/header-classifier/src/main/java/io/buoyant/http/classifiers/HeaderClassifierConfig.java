e4c8c9a372ed3001b109274a4b4e7e7a0571e7bb
Alex Leong <alex@buoyant.io>
2016-06-28 13:12:05 -0700

    Add comments to example plugin. (#22)

--- plugins/header-classifier/src/main/java/io/buoyant/http/classifiers/HeaderClassifierConfig.java
+++ plugins/header-classifier/src/main/java/io/buoyant/http/classifiers/HeaderClassifierConfig.java
@@ -6,9 +6,18 @@ import com.twitter.finagle.service.ResponseClass;
 import io.buoyant.linkerd.ResponseClassifierConfig;
 import scala.PartialFunction;
 
+/**
+ * HeaderClassifierConfig defines the structure of the config block for this
+ * plugin and constructs the response classifier.
+ */
 public class HeaderClassifierConfig implements ResponseClassifierConfig {
+
+    /* This public member is populated by the json property of the same name. */
     public String headerName;
 
+    /**
+     * Construct the repsonse classifier.
+     */
     @Override
     @JsonIgnore
     public PartialFunction<ReqRep, ResponseClass> mk() {
--- plugins/header-classifier/src/main/java/io/buoyant/http/classifiers/HeaderClassifierInitializer.java
+++ plugins/header-classifier/src/main/java/io/buoyant/http/classifiers/HeaderClassifierInitializer.java
@@ -2,6 +2,16 @@ package io.buoyant.http.classifiers;
 
 import io.buoyant.linkerd.ResponseClassifierInitializer;
 
+/**
+ * This config initializer is loaded by linkerd at startup and registers the
+ * `HeaderClassifierConfig` class under the id "io.buoyant.headerClassifier".
+ * This tells linkerd's config system to deserialize response classifier config
+ * blocks to `HeaderClassifierConfig` if the kind is
+ * "io.buoyant.headerClassifer".
+ *
+ * In order for linkerd to load this class, it must be listed in the
+ * `META-INF/services/io.l5d.linkerd.ResponseClassifierInitializer` file.
+ */
 public class HeaderClassifierInitializer extends ResponseClassifierInitializer {
     @Override
     public String configId() {
