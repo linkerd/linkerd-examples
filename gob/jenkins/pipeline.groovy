ad8421795bce27c39755163af402763e70f7f58d
Sarah Brown <sarah@buoyant.io>
2016-10-28 11:54:27 -0700

    better groovy script comments

--- gob/jenkins/pipeline.groovy
+++ gob/jenkins/pipeline.groovy
@@ -1,3 +1,4 @@
+// These constants could also be defined as [build parameters](https://wiki.jenkins-ci.org/display/JENKINS/Parameterized+Build)
 def namerd = "100.100.100.10"        // The host used by namerctl to reach the namerd cluster.
 def namerdNamespace = "default"      // The namerd namespace of the dtab we'll be updating.
 def k8sNamespace = "sb-jenkins-test" // The k8s namespace that the services are running in.
@@ -9,28 +10,34 @@ def prefix = "/host/${logicalName}"  // The dtab prefix for the dentry we want t
 
 node {
     stage "auth"
+    // Add the $GOPATH to $PATH if it's not there already.
     env.PATH = "${env.GOPATH}/bin:${env.PATH}"
+    // Check out this repo in order to use the k8s yml in gob/k8s/gen.
     git branch: 'esbie/sb-jenkins-test', url: 'https://github.com/BuoyantIO/linkerd-examples.git'
-
+    // Authenticate against gcloud with a service account.
     sh "gcloud auth activate-service-account --key-file credentials.json --project buoyant-hosted"
     sh "gcloud docker --authorize-only"
     sh "gcloud config set compute/zone us-central1-b"
 
+    // Save the original dst in case we need to rollback.
     def originalDst = getDstForPrefix(prefix, namerd, namerdNamespace)
 
-    stage "canary"
+    stage "deploy"
+    // Signal our intent to deploy, in case someone else is trying to edit the dtab at the same time.
     signalDeploy(prefix, namerd, namerdNamespace)
+    // Deploy the new service!
     dir('gob') {
         sh "kubectl apply -f k8s/gen"
     }
-    sleep 5 // give the instance some time to start
+    // give the instance some time to start
+    sleep 5
 
     stage "integration testing"
-    def dtabOverride = "Dtab-local: ${prefix} => /tmp/${newVersion}"
+    def dtabOverride = "l5d-dtab: ${prefix} => /tmp/${newVersion}"
     runIntegrationTests(webFrontend, dtabOverride)
     input message: "Integration tests successful! You can reach the service with `curl -H '${dtabOverride}' '${webFrontend}'`", ok: 'OK, done with manual testing step'
 
-    stage "start rolling deploy (10%)"
+    stage "shift traffic (10%)"
     updateDentry(prefix, "1 * /tmp/${newVersion} & 9 * /tmp/${currentVersion}", false, namerd, namerdNamespace)
     try {
         input message: "Shifting 10% of traffic. To view, open: ${webFrontend}:9990", ok: 'OK, success rates look stable'
@@ -40,8 +47,8 @@ node {
         throw err
     }
 
-    stage "complete rolling deploy (100%)"
-    updateDentry(prefix, "/tmp/${newVersion} | /tmp/${currentVersion}", false, namerd, namerdNamespace)
+    stage "shift traffic (100%)"
+    updateDentry(prefix, "/tmp/${newVersion}", false, namerd, namerdNamespace)
     input message: "Ready to cleanup?", ok: 'Yep, everything looks good'
 
     stage "cleanup"
@@ -50,6 +57,7 @@ node {
     sh "kubectl delete rc ${currentVersion} --namespace=${k8sNamespace}"
 }
 
+// The world's simplest integration test
 def runIntegrationTests(webFrontend, dtabOverride) {
     def resp = sh(script: "curl -sL -w \"%{http_code}\" -H '${dtabOverride}' \"${webFrontend}/gob?limit=10\" -o /dev/null", returnStdout: true).trim()
     if (resp != "200") {
@@ -57,6 +65,7 @@ def runIntegrationTests(webFrontend, dtabOverride) {
     }
 }
 
+// Calls namerctl dtab get through the pipeline sh step and returns the result.
 def getDtab(namerd, namerdNamespace) {
     return sh (script: "namerctl dtab get ${namerdNamespace} --base-url=http://${namerd} --json", returnStdout: true)
 }
@@ -66,6 +75,7 @@ def getDstForPrefix(prefix, namerd, namerdNamespace) {
     return getDst(prefix, jsonResp)
 }
 
+// Stops deploy if dst is already marked with /tmp
 def signalDeploy(prefix, namerd, namerdNamespace) {
     def jsonResp = getDtab(namerd, namerdNamespace)
     if (isTmp(prefix, jsonResp)) {
@@ -81,6 +91,7 @@ def updateDentry(prefix, dst, replace, namerd, namerdNamespace) {
     updateDtab(updatedResp, namerd, namerdNamespace)
 }
 
+// Calls namerctl dtab update through the pipeline sh step and echoes the resulting dtab
 def updateDtab(serializedJson, namerd, namerdNamespace) {
     writeFile file: 'default.dtab', text: serializedJson
     sh "namerctl dtab update ${namerdNamespace} default.dtab --base-url=http://${namerd} --json"
@@ -88,6 +99,7 @@ def updateDtab(serializedJson, namerd, namerdNamespace) {
     echo "dtab now updated to ${dtab}"
 }
 
+// Finds a matching prefix, and checks if it contains tmp
 @NonCPS
 def isTmp(prefix, jsonResp) {
     def json = new groovy.json.JsonSlurper().parseText(jsonResp)
@@ -101,6 +113,7 @@ def isTmp(prefix, jsonResp) {
     return false
 }
 
+// Finds a matching prefix, and replaces srv with tmp
 @NonCPS
 def markAsTmp(prefix, jsonResp) {
     def json = new groovy.json.JsonSlurper().parseText(jsonResp)
@@ -113,6 +126,7 @@ def markAsTmp(prefix, jsonResp) {
     return groovy.json.JsonOutput.toJson(dtab)
 }
 
+// Finds a matching prefix, and replaces its dst
 @NonCPS
 def replaceDst(prefix, dst, jsonResp) {
     def json = new groovy.json.JsonSlurper().parseText(jsonResp)
@@ -125,6 +139,7 @@ def replaceDst(prefix, dst, jsonResp) {
     return groovy.json.JsonOutput.toJson(dtab)
 }
 
+// Finds a matching prefix, and adds dst as an alternate
 @NonCPS
 def addToDst(prefix, dst, jsonResp) {
     def json = new groovy.json.JsonSlurper().parseText(jsonResp)
@@ -137,6 +152,7 @@ def addToDst(prefix, dst, jsonResp) {
     return groovy.json.JsonOutput.toJson(dtab)
 }
 
+// Finds a matching prefix, and returns it's dst
 @NonCPS
 def getDst(prefix, jsonResp) {
     def json = new groovy.json.JsonSlurper().parseText(jsonResp)
