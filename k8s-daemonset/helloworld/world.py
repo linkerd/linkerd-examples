fa83500174a52b3a22928d7714ac28ec700a2ce3
Kevin Lingerfelt <kl@buoyant.io>
2016-11-02 16:06:47 -0700

    Add k8s jenkins config, setup world service for CD

--- k8s-daemonset/helloworld/world.py
+++ k8s-daemonset/helloworld/world.py
@@ -4,10 +4,11 @@ import os
 app = Flask(__name__)
 
 pod_ip = os.getenv("POD_IP")
+target_world = os.getenv("TARGET_WORLD", "world")
 
 @app.route("/")
 def world():
-    return "world (" + pod_ip + ")!"
+    return target_world + " (" + pod_ip + ")!"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=7778)
